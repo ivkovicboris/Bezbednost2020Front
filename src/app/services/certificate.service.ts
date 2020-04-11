7cab771ceacbbd4c72c960344e3a4da7162746c3
LosBlancos6 <stefans996@gmail.com>
2020-04-11 15:13:23 +0200

    create root certificate

---
+++ src/app/services/certificate.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CertificateService {
+
+  private baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public createCertificate(body): Observable<any> {
+    return this.http.post(this.baseUrl + 'certificate/create', body);
+  }
+
+}
